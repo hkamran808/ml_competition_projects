6f2e7a63434bab709e40e7d5da709e1487881957
Kamran Huseynli <k.huseynli@ufaz.az>
2026-01-28 05:22:48 +0400

    Dataset checking for inconsistencies or etc...

--- Tiny_City_Drone_Delivery_Predictor.py
+++ Tiny_City_Drone_Delivery_Predictor.py
@@ -6,3 +6,10 @@ test_df = pd.read_csv("project1_test.csv")
 
 print(train_df.head())
 print(train_df.info())
+
+"""
+Doing theoretical parts: Dataset size & sanity check
+"""
+print("Missing values for train:\n", train_df.isna().sum())
+print("Missing values for test:\n", test_df.isna().sum())
+print("Shape of datasets: ", train_df.shape, test_df.shape)
