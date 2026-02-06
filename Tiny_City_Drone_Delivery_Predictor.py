9199e79d9393c3e19a66b151ecd8ea064b1bab43
Kamran Huseynli <k.huseynli@ufaz.az>
2026-02-07 01:28:47 +0400

    Day 5: Built and evaluated baseline supervised model with proper train-validation split

--- Tiny_City_Drone_Delivery_Predictor.py
+++ Tiny_City_Drone_Delivery_Predictor.py
@@ -115,3 +115,24 @@ def altitude_features(altitude_list):
     return pd.Series([altitude_len, altitude_max, altitude_min, 
                       altitude_range, altitude_mean, altitude_std, 
                       altitude_upward_sum, altitude_downward_sum])
+
+# modeling part
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+x = train_df.drop("target")
+y = train_df["target"]
+
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+model.fit(X_train, Y_train)
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(Y_test, predictions)
+print("Accuracy:", accuracy)
+print("classification_report: ", classification_report(Y_test, predictions))
+print("confusion_matrix: ", confusion_matrix(Y_test, predictions))
+print(10*"-", "Modeling completed!", 10*"-")
+
+#residuals = Y_test - predictions
