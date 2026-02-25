0756d8cc49af99262209a818685b2e7c93785f80
Kamran Huseynli <k.huseynli@ufaz.az>
2026-02-26 02:01:52 +0400

    Day20: Initial EDA and dataset inspection for Home Credit dataset >> >> - Loaded application_train.csv and application_test.csv - Checked shapes, column names, and head of datasets >> >> - Identified categorical and numerical features >> >> - Checked for missing values in both train and test sets >> >> - Defined variables and Train/Test Split for now...

---
+++ Home_Credit_Default_Risk.py
@@ -0,0 +1,40 @@
+"""
+Project 3: Home Credit Default Risk
+
+- Dataset: application_train.csv
+        application_test.csv
+        more to be added...
+
+- Goal: Building perfect model using pipeline and feature engineering correctly. learning all possible outcomes and reasons to be better at competitions and such working environments
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the datasets
+train_df = pd.read_csv("application_train.csv")
+test_df = pd.read_csv("application_test.csv")
+
+print("Training data shape:", train_df.shape)
+print("Testing data shape:", test_df.shape)
+print("Training data columns:", train_df.columns)
+print("Testing data columns:", test_df.columns)
+print(train_df.head())
+
+# Check for missing values
+print("Missing values in training data:\n", train_df.isnull().sum())
+print("Missing values in testing data:\n", test_df.isnull().sum())
+
+print("Categorical features in training data:\n", train_df.select_dtypes(include=["object"]).columns)
+print("Categorical features in testing data:\n", test_df.select_dtypes(include=["object"]).columns)
+print("Numerical features in training data:\n", train_df.select_dtypes(include=["number"]).columns)
+print("Numerical features in testing data:\n", test_df.select_dtypes(include=["number"]).columns)
+
+# defining target variable and features
+x = train_df.drop(columns=["TARGET"])
+y = train_df["TARGET"]
+
+# train test split
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
--- ML_Roadmap_Last_Project.py
+++ ML_Roadmap_Last_Project.py
@@ -1,5 +1,23 @@
 """
-Project 3: ...
-- Dataset: ...
-- Goal: Building perfect model using pipeline and feature engineering correctly. learning all possible outcomes and reasons to be better at competitions and such workings
+Project 3: Home Credit Default Risk
+
+- Dataset: application_train.csv
+        application_test.csv
+        more to be added...
+
+- Goal: Building perfect model using pipeline and feature engineering correctly. learning all possible outcomes and reasons to be better at competitions and such working environments
 """
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the datasets
+train_df = pd.read_csv("application_train.csv")
+test_df = pd.read_csv("application_test.csv")
+
+print("Training data shape:", train_df.shape)
+print("Testing data shape:", test_df.shape)
+print("Training data columns:", train_df.columns)
+print("Testing data columns:", test_df.columns)
+print(train_df.head())
