39f80227fa1c73baf44620bbc80a15c103037d48
Kamran Huseynli <k.huseynli@ufaz.az>
2026-03-01 00:59:49 +0400

    Day21: Almost finished - it has some minor issues like safety checks

--- Home_Credit_Default_Risk.py
+++ Home_Credit_Default_Risk.py
@@ -31,21 +31,50 @@ print("Categorical features in testing data:\n", test_df.select_dtypes(include=[
 print("Numerical features in training data:\n", train_df.select_dtypes(include=["number"]).columns)
 print("Numerical features in testing data:\n", test_df.select_dtypes(include=["number"]).columns)
 
-# defining target variable and features
-x = train_df.drop(columns=["TARGET"])
+# Missing Value Intelligence (per column)
+missing_values = train_df.isnull().sum()
+missing_percentages = (missing_values / len(train_df)) * 100
+missing_values_df = pd.DataFrame({"Missing Count": missing_values, "Percentage": missing_percentages})
+missing_df = missing_values_df[missing_values_df["Missing Count"] > 0].sort_values(by="Percentage", ascending=False)
+print(10*"-", "Missing values per column in training data (sorted by percentage)", 10*"-" + "\n", missing_df)
+
+# For each column with >30% missing: we can consider dropping them or using advanced imputation techniques
+for col in missing_df[missing_df["Percentage"] > 30].index:
+    train_df[col + "_IS_MISSING"] = train_df[col].isnull().astype(int)
+
+# credit - income - annuity ratios
+train_df["CREDIT_INCOME_RATIO"] = train_df["AMT_CREDIT"] / train_df["AMT_INCOME_TOTAL"]
+train_df["ANNUITY_INCOME_RATIO"] = train_df["AMT_ANNUITY"] / train_df["AMT_INCOME_TOTAL"]
+train_df["CREDIT_ANNUITY_RATIO"] = train_df["AMT_CREDIT"] / train_df["AMT_ANNUITY"]
+
+# income per child & employment ratio (with anomaly value)
+train_df["INCOME_PER_CHILD"] = train_df["AMT_INCOME_TOTAL"] / (train_df["CNT_CHILDREN"] + 1)
+train_df["EMPLOYED_ANOMALY"] = (train_df["DAYS_EMPLOYED"] == 365243).astype(int)
+train_df["DAYS_EMPLOYED"] = train_df["DAYS_EMPLOYED"].replace(365243, np.nan)
+train_df["DAYS_EMPLOYED_RATIO"] = (train_df["DAYS_EMPLOYED"]).abs() / (train_df["DAYS_BIRTH"]).abs()
+
+if train_df.columns.str.startswith("FLAG_DOCUMENT_").any():
+    columns = train_df.columns[train_df.columns.str.startswith("FLAG_DOCUMENT_")]
+    train_df[columns] = train_df[columns].abs()
+    train_df["TOTAL_DOCUMENT_FLAGS"] = train_df[columns].sum(axis=1)
+
+# redefining target variable and features after doing it pre feature engineering for checking the target imbalance and such
+x = train_df.drop(columns=["TARGET", "SK_ID_CURR"])
 y = train_df["TARGET"]
 
 # target imbalance check
 print("Target value counts:\n", y.value_counts())
 print("Target value proportions:\n", y.value_counts(normalize=True))
 
-# Missing Value Intelligence (per column) % missing per column (sorted descending)
-missing_values = x.isnull().sum()
-missing_percentages = (missing_values / len(x)) * 100
-missing_values_df = pd.DataFrame({"Missing Count": missing_values, "Percentage": missing_percentages})
-missing_df = missing_values_df[missing_values_df["Missing Count"] > 0].sort_values(by="Percentage", ascending=False)
-print(10*"-", "Missing values per column in training data (sorted by percentage)", 10*"-" + "\n", missing_df)
+# Handling missing values: Impute with median for numerical features and mode for categorical features if the percentage of missing values is less than 30%
+columns = missing_df[missing_df["Percentage"] <= 30]
+if columns.columns.dtypes == "object":
+    train_df[columns] = train_df[columns].fillna(train_df[columns].mode().iloc[0])
+elif columns.columns.dtypes == "number":
+    train_df[columns] = train_df[columns].fillna(train_df[columns].median())
 
+"""
 # train test split
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
+"""
